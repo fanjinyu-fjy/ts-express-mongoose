458d8636a3ab17a01504e04d5c039e23dd845c81
huanyueyaoqin <huanyueyaoqin@qq.com>
2020-03-02 12:03:45 +0800

    add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("ts");
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+app.get("/", (_req: Request, res: Response) => {
+  res.send("hello word");
+});
+
+const port: any = process.env.PORT || 6060;
+
+const main = () => {
+  app.listen(port, () => {
+    console.log(`Server is Running at ${port}`);
+  });
+};
+main();
