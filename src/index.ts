7dc87ab750df08ff6f73fc6831f83fd11c3be834
huanyueyaoqin <huanyueyaoqin@qq.com>
2020-03-03 10:27:38 +0800

    错误处理

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
 import mongoose from "mongoose";
+import { NOT_FOUND } from "http-status-codes";
+import HttpException from "./exceptions/HttpException";
+import errorMiddleware from "./middlewares/error.middleware";
 
 const app: Express = express();
+
 app.get("/", (_req: Request, res: Response) => {
-  res.send("hello word");
+  res.json({
+    message: "hello world"
+  });
 });
+
+app.use((_req: Request, _res: Response, next: NextFunction) => {
+  const error: HttpException = new HttpException(NOT_FOUND, "Router Not Found");
+  next(error);
+});
+
+app.use(errorMiddleware);
 
 const port: any = process.env.PORT || 6060;
 
---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,21 @@
+import HttpException from "src/exceptions/HttpException";
+import { Request, Response, NextFunction } from "express";
+import { INTERNAL_SERVER_ERROR } from "http-status-codes";
+
+const errorMiddleware = (
+  error: HttpException,
+  _req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const status = error.status || INTERNAL_SERVER_ERROR;
+  const message = error.message || "Something Error Happened";
+
+  res.status(status).json({
+    success: false,
+    message
+  });
+  next();
+};
+
+export default errorMiddleware;
